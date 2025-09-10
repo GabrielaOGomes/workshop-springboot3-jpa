853ef432dec1e93396446af70b3906e50dd225df
Gabriela Oliveira <gabriela.gomestrab@gmail.com>
2025-09-10 17:39:27 -0300

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -26,4 +26,9 @@ public class UserService {
         Optional<User> obj = repository.findById(id);
         return obj.get();
     }
+
+    //inserir no banco de dados novo usuário
+    public User insert(User obj) {
+        return repository.save(obj);
+    }
 }
